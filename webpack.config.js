cf90b4f7171ab6e54138618dd1acc559013fcac6
Cyril CHAPON <cyril@permettezmoideconstruire.fr>
2018-03-19 08:29:03 +0100

    Switch babel from 6 to 7beta

--- webpack.config.js
+++ webpack.config.js
@@ -32,12 +32,7 @@ const config = {
       {
         test: /(\.jsx|\.js)$/,
         loader: 'babel-loader',
-        exclude: /(node_modules|bower_components)/
-      },
-      {
-        test: /(\.jsx|\.js)$/,
-        loader: 'eslint-loader',
-        exclude: /node_modules/
+        exclude: /(node_modules)/
       }
     ]
   },
